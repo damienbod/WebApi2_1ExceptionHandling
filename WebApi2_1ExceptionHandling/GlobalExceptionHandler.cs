f7c8c75b0e650194d91583e37cd0afc8e396e9a0
damienbod <damien_bod@hotmail.com>
2014-05-25 18:32:23 +0200

    corrected global exception handling example

--- WebApi2_1ExceptionHandling/GlobalExceptionHandler.cs
+++ WebApi2_1ExceptionHandling/GlobalExceptionHandler.cs
@@ -1,8 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Net.Cache;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
 
 namespace WebApi2_1ExceptionHandling
 {
@@ -17,8 +21,30 @@ namespace WebApi2_1ExceptionHandling
                     Content = new StringContent(context.Exception.Message),
                     ReasonPhrase = "ValidationException"
                 };
-                throw new HttpResponseException(resp);
 
+                context.Result = new ErrorMessageResult(context.Request, resp);
+            }
+            else
+            {
+                // Do something here...
+            }
+        }
+
+        public class ErrorMessageResult : IHttpActionResult
+        {
+            private HttpRequestMessage _request;
+            private HttpResponseMessage _httpResponseMessage;
+
+
+            public ErrorMessageResult(HttpRequestMessage request, HttpResponseMessage httpResponseMessage)
+            {
+                _request = request;
+                _httpResponseMessage = httpResponseMessage;
+            }
+
+            public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+            {
+                return Task.FromResult(_httpResponseMessage);
             }
         }
     }
